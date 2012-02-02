7ab2b14dd85e19963d4d004202a33c2051bed15e
Vijay Parthasarathy <vijay2win@gmail.com>
2012-02-02 13:02:39 -0800

    Adding DeleteSampler

---
+++ src/main/java/com/netflix/jmeter/sampler/DeleteSampler.java
@@ -0,0 +1,13 @@
+package com.netflix.jmeter.sampler;
+
+public class DeleteSampler extends AbstractSampler
+{
+    private static final long serialVersionUID = -2103499609822848595L;
+
+    public ResponseData execute() throws OperationException
+    {
+        Operation ops = Connection.getInstace().newOperation(getColumnFamily());
+        setSerializers(ops);
+        return ops.delete(getKey(), getColumnName());
+    }
+}
