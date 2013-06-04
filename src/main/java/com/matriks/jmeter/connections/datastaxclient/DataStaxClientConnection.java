b1d567449564b6cca48b2681a14fcf090697d5b2
Koray Sariteke <koray.sariteke@matriksdata.com>
2013-06-04 11:01:57 +0300

    commit getcomposite functionality with datastax java api

---
+++ src/main/java/com/matriks/jmeter/connections/datastaxclient/DataStaxClientConnection.java
@@ -0,0 +1,65 @@
+package com.matriks.jmeter.connections.datastaxclient;
+
+import java.io.File;
+import java.io.FileReader;
+import java.util.Properties;
+
+import org.apache.commons.lang.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.datastax.driver.core.Cluster;
+import com.datastax.driver.core.Session;
+import com.netflix.jmeter.connections.a6x.AstyanaxConnection;
+import com.netflix.jmeter.sampler.Connection;
+import com.netflix.jmeter.sampler.Operation;
+
+public class DataStaxClientConnection extends Connection {
+	private static final Logger logger = LoggerFactory.getLogger(AstyanaxConnection.class);
+	private Session session;
+	private Cluster cluster;
+	public static final DataStaxClientConnection instance = new DataStaxClientConnection();
+	public Properties config = new Properties();
+
+	public Session session() {
+		if (session != null)
+			return session;
+
+		synchronized (DataStaxClientConnection.class) {
+			if (session != null)
+				return session;
+
+			try {
+				File propFile = new File("cassandra.properties");
+				if (propFile.exists()) {
+					config.load(new FileReader(propFile));
+				}
+				
+				cluster = Cluster.builder().addContactPoints(StringUtils.join(endpoints, ":" + port)).build();
+				session = cluster.connect();
+				session.execute("USE " + getKeyspaceName() + ";");
+				return session;
+			}
+			catch (Exception e) {
+				throw new RuntimeException(e);
+			}
+		}
+	}
+
+	@Override
+	public Operation newOperation(String columnFamily, boolean isCounter) {
+		return new DataStaxClientOperation(columnFamily, isCounter);
+	}
+
+	@Override
+	public String logConnections() {
+		return cluster == null ? "" : "";
+	}
+
+	@Override
+	public void shutdown() {
+		if (cluster != null)
+			cluster.shutdown();
+	}
+
+}
---
+++ src/main/java/com/matriks/jmeter/connections/datastaxclient/DataStaxClientOperation.java
@@ -0,0 +1,119 @@
+package com.matriks.jmeter.connections.datastaxclient;
+
+import java.nio.ByteBuffer;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
+import com.datastax.driver.core.ResultSet;
+import com.datastax.driver.core.Row;
+import com.datastax.driver.core.Session;
+import com.datastax.driver.core.querybuilder.QueryBuilder;
+import com.netflix.astyanax.connectionpool.OperationResult;
+import com.netflix.astyanax.model.ColumnFamily;
+import com.netflix.astyanax.serializers.AbstractSerializer;
+import com.netflix.jmeter.sampler.AbstractSampler.ResponseData;
+import com.netflix.jmeter.sampler.Operation;
+import com.netflix.jmeter.sampler.OperationException;
+import com.netflix.jmeter.utils.SystemUtils;
+
+public class DataStaxClientOperation implements Operation {
+	private AbstractSerializer valueSerializer;
+	private ColumnFamily<Object, Object> cfs;
+	private AbstractSerializer columnSerializer;
+	private final String cfName;
+	private final boolean isCounter;
+
+	public class DataStaxClientResponseData extends ResponseData {
+		public DataStaxClientResponseData(String response, int size, String host, long latency, Object key, Object cn, Object value) {
+			super(response, size, host, latency, key, cn, value);
+		}
+
+		public DataStaxClientResponseData(String response, int size, OperationResult<?> result, Object key, Object cn, Object value) {
+			super(response, size, EXECUTED_ON + (result != null ? result.getHost().getHostName() : ""), (result != null ? result.getLatency(TimeUnit.MILLISECONDS) : 0), key, cn, value);
+		}
+
+		public DataStaxClientResponseData(String response, int size, OperationResult<?> result, Object key, Map<?, ?> kv) {
+			super(response, size, (result == null) ? "" : result.getHost().getHostName(), result != null ? result.getLatency(TimeUnit.MILLISECONDS) : 0, key, kv);
+		}
+	}
+
+	public DataStaxClientOperation(String cfName, boolean isCounter) {
+		this.cfName = cfName;
+		this.isCounter = isCounter;
+	}
+
+	@Override
+	public void serlizers(AbstractSerializer<?> keySerializer, AbstractSerializer<?> columnSerializer, AbstractSerializer<?> valueSerializer) {
+		this.cfs = new ColumnFamily(cfName, keySerializer, columnSerializer);
+		this.columnSerializer = columnSerializer;
+		this.valueSerializer = valueSerializer;
+	}
+
+	@Override
+	public ResponseData put(Object key, Object colName, Object value) throws OperationException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ResponseData batchMutate(Object key, Map<?, ?> nv) throws OperationException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ResponseData get(Object rkey, Object colName) throws OperationException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ResponseData rangeSlice(Object rKey, Object startColumn, Object endColumn, boolean reversed, int count) throws OperationException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ResponseData putComposite(String key, String colName, ByteBuffer vbb) throws OperationException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ResponseData batchCompositeMutate(String key, Map<String, ByteBuffer> nv) throws OperationException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ResponseData getComposite(String key, String compositeColName) throws OperationException {
+		StringBuffer response = new StringBuffer();
+		Session session = DataStaxClientConnection.instance.session();
+
+		String[] partitionKeyList = key.split(":");
+		String partitionKey = partitionKeyList[0];
+		String partitionValue = partitionKeyList[1];
+
+		String[] colList = compositeColName.split(":");
+		String clusteredKey = colList[0];
+		String clusteredKeyValue = colList[1];
+		String colName = colList[2];
+
+		ResultSet rs = session.execute(QueryBuilder.select(colName).from(cfName).where(QueryBuilder.eq(partitionKey, partitionValue)).and(QueryBuilder.eq(clusteredKey, clusteredKeyValue))
+				.enableTracing());
+		Row row = rs.one();
+
+		String value = SystemUtils.convertToString(valueSerializer, row.getBytesUnsafe(colName));
+		response.append(value);
+
+		return new DataStaxClientResponseData(response.toString(), 0, "", TimeUnit.MILLISECONDS.convert(rs.getExecutionInfo().getQueryTrace().getDurationMicros(), TimeUnit.MICROSECONDS), key,
+				compositeColName, null);
+	}
+
+	@Override
+	public ResponseData delete(Object rkey, Object colName) throws OperationException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
